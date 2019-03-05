25d88a9821c5a7c5a59eff21c73af4c0281f96e6
devops <rajesh.m712@gmail.com>
2019-03-05 18:22:48 +0530

    added a new line

--- src/sample.java
+++ src/sample.java
@@ -2,5 +2,6 @@ public class sample
 {
     public static void main(String[] args) {
         System.out.println("hello world welcome");
+        System.out.println("this is anew beginner project");
     }
 }
